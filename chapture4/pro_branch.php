f03e3fc68a3250b207a7e7a3ee85fdf5168c2972
SoranoFukuzo <fukuzo9431@gmail.com>
2025-03-18 05:40:59 +0900

    chapture4_2

--- chapture4/pro_branch.php
+++ chapture4/pro_branch.php
@@ -1,68 +1,61 @@
 <?php
 
-session_start();
-session_regenerate_id(true);
-
-if( isset($_SESSION['login']) == false )
-{
-	print 'ログインされていません。<br />';
-	print '<a href="staff_login.html">ログイン画面へ</a>';
+//if(isset($_POST['disp'])==true)
+if( filter_input( INPUT_POST, 'disp' ) == true ){
+	//if(isset($_POST['procode'])==false)
+        if( filter_input( INPUT_POST, 'procode' ) == false ) {
+            //
+            // 商品が選択されていないとき
+            // オプションボタンが未選択
+            //
+            header( 'Location:pro_ng.php' );
+            exit();
+	}
+        
+	//$pro_code=$_POST['procode'];
+        $pro_code = filter_input( INPUT_POST, 'procode' );
+        
+	header( 'Location:pro_disp.php?procode=' . $pro_code );
+        
 	exit();
 }
 
-//if(isset($_POST['disp'])==true)
-if(filter_input( INPUT_POST, 'disp' )==true)
-{
-	//if(isset($_POST['procode'])==false)
-    if(filter_input( INPUT_POST, 'procode' )==false)
-    {
-		header('Location:pro_ng.php');
-		exit();
-    }
-    //$pro_code=$_POST['procode'];
-    $pro_code=filter_input( INPUT_POST, 'procode' );
-        
-    header('Location:pro_disp.php?procode=' . $pro_code );
-    exit();
-}
-
 //if(isset($_POST['add'])==true)
-if(filter_input( INPUT_POST, 'add' ) == true )   
-{
-	header('Location:pro_add.php');
+if( filter_input( INPUT_POST, 'add' ) == true ) {
+	header( 'Location:pro_add.php' );
 	exit();
 }
 
 //if(isset($_POST['edit'])==true)
-if( filter_input( INPUT_POST, 'add' ) == true )
-{
-	//if(isset($_POST['procode'])==false)
-        if( filter_input( INPUT_POST, 'procode' ) == false )
-	{
-		header('Location:pro_ng.php');
-		exit();
-	}
-	
-        //$pro_code=$_POST['procode'];
-        $pro_code= filter_input( INPUT_POST, 'procode' );
-        
-	header('Location:pro_edit.php?procode=' . $pro_code);
-	exit();
-}
-
-//if(isset($_POST['delete'])==true)
-if(filter_input( INPUT_POST, 'delete' ) == true ) {
+if( filter_input( INPUT_POST, 'edit' ) == true ) {
     
 	//if(isset($_POST['procode'])==false)
-        if(filter_input( INPUT_POST, 'procode' ) == false ) {
-		header('Location:pro_ng.php');
+        if( filter_input( INPUT_POST, 'procode' ) == false ) {
+		header( 'Location:pro_ng.php' );
 		exit();
 	}
         
 	//$pro_code=$_POST['procode'];
         $pro_code = filter_input( INPUT_POST, 'procode' );
         
-	header('Location:pro_delete.php?procode='.$pro_code);
+	header( 'Location:pro_edit.php?procode=' . $pro_code );
 	exit();
+        
 }
 
+//if(isset($_POST['delete'])==true)
+if( filter_input( INPUT_POST, 'delete' ) == true ) {
+    
+	//if(isset($_POST['procode'])==false)
+        if( filter_input( INPUT_POST, 'procode' ) == false ) {
+            
+		header( 'Location:pro_ng.php' );
+		exit();
+	}
+        
+	//$pro_code=$_POST['procode'];
+        $pro_code = filter_input( INPUT_POST, 'procode' );
+        
+	header( 'Location:pro_delete.php?procode=' . $pro_code );
+	exit();
+}
